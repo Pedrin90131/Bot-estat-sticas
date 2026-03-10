024a756ba78edaf7b63aa2f80fc70cfe366ea1e8
Pedrin90131 <87963127+Pedrin90131@users.noreply.github.com>
2026-03-09 21:14:05 -0300

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd $RENDER_SRC_ROOT
-pip install -r requirements.txt
-python main.py
+# Acha o requirements e instala onde quer que ele esteja
+pip install -r $(find . -name requirements.txt)
+# Acha o main.py e executa
+python $(find . -name main.py)
+
 
 
